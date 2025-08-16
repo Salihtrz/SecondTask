4c5845a33e4876fa6da1c2d4279ccd0fa446bdf9
Salih <salihterzi81@gmail.com>
2025-08-16 17:25:56 +0300

    Tested
    latest version tested

--- SecondTask/SecondTask.Persistence/obj/Debug/net9.0/SecondTask.Persistence.AssemblyInfo.cs
+++ SecondTask/SecondTask.Persistence/obj/Debug/net9.0/SecondTask.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondTask.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+858be94e46cf3833cab25ce3787e15060b9f6fef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef80422afd13ecd371cb68c64e5173c1bf883918")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondTask.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondTask.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
