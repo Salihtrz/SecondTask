f5cc8d67b925c76f8c7b4f0add8430760ad12326
Salih <salihterzi81@gmail.com>
2025-08-16 17:46:34 +0300

    Directory
    Project local directory changed

---
+++ SecondTask.Persistence/Repositories/Repository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecondTask.Application.Interfaces;
+using SecondTask.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask.Persistence.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly context _context;
+
+        public Repository(context context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateAsync(T t)
+        {
+            _context.Set<T>().Add(t);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(T t)
+        {
+            _context.Set<T>().Remove(t);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T>? GetByFilterAsync(Expression<Func<T, bool>> filter)
+        {
+            return await _context.Set<T>().SingleOrDefaultAsync(filter);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T t)
+        {
+            _context.Set<T>().Update(t);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ SecondTask.WebUI/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Login/Index/";
+    opt.Cookie.SameSite = SameSiteMode.Strict;
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "SecondTaskJwt";
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
